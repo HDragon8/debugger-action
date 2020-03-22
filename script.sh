e71085a5278794a174e334b752d384f53a261a84
noodles <houyonghua2015@gmail.com>
2020-03-22 13:15:09 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ if [ -x "$(command -v brew)" ]; then
   brew install tmate > /tmp/brew.log
 fi
 if [ -x "$(command -v apt-get)" ]; then
-  sudo apt-get install -y tmate openssh-client > /tmp/apt-get.log
+  curl -fsSL git.io/tmate.sh | bash
 fi
 
 # Generate ssh key if needed
@@ -25,11 +25,16 @@ tmate -S /tmp/tmate.sock new-session -d
 tmate -S /tmp/tmate.sock wait tmate-ready
 
 # Print connection info
-echo ________________________________________________________________________________
-echo
-echo To connect to this session copy-n-paste the following into a terminal:
-tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
-echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
+DISPLAY=1
+while [ $DISPLAY -le 3 ]; do
+  echo ________________________________________________________________________________
+  echo To connect to this session copy-n-paste the following into a terminal or browser:
+  tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+  tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
+  [ ! -f /tmp/keepalive ] && echo -e "After connecting you can run 'touch /tmp/keepalive' to disable the 30m timeout"
+  DISPLAY=$(($DISPLAY+1))
+  sleep 30
+done
 
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
   MSG=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
@@ -37,7 +42,7 @@ if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
 fi
 
 # Wait for connection to close or timeout in 15 min
-timeout=$((15*60))
+timeout=$((30*60))
 while [ -S /tmp/tmate.sock ]; do
   sleep 1
   timeout=$(($timeout-1))
@@ -45,7 +50,7 @@ while [ -S /tmp/tmate.sock ]; do
   if [ ! -f /tmp/keepalive ]; then
     if (( timeout < 0 )); then
       echo Waiting on tmate connection timed out!
-      exit 1
+      sudo init 0
     fi
   fi
 done
